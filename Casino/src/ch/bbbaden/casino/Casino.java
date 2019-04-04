543a6b6fd7d83b61ce1c077897f7dfe74b776bc9
Dan6erbond <moravrav@gmail.com>
2019-04-04 14:16:30 +0200

    created scene-manager
    changeScene() uses the current (main) window
    openWindow() opens a new (unsaved) window

--- Casino/src/ch/bbbaden/casino/Casino.java
+++ Casino/src/ch/bbbaden/casino/Casino.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package casino;
+package ch.bbbaden.casino;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -12,19 +13,13 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
- *
  * @author User
  */
 public class Casino extends Application {
-    
+
     @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
-        
-        Scene scene = new Scene(root);
-        
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage stage) throws IOException {
+        SceneManager.getInstance().changeScene("Login.fxml");
     }
 
     /**
@@ -33,5 +28,5 @@ public class Casino extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
---
+++ Casino/src/ch/bbbaden/casino/SceneManager.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.bbbaden.casino;
+
+import java.io.IOException;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+/**
+ *
+ * @author User
+ */
+public class SceneManager {
+    
+    private static SceneManager sceneManager;
+    private Stage stage;
+    
+    private SceneManager(){
+        if (stage == null){
+            stage = new Stage(StageStyle.UTILITY);
+        }
+        sceneManager = this;
+    }
+    
+    public static SceneManager getInstance(){
+        if (sceneManager == null){
+            sceneManager = new SceneManager();
+        }
+        return sceneManager;
+    }
+    
+    public void changeScene(String fxml) throws IOException{        
+        Parent root = FXMLLoader.load(getClass().getResource(fxml));
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+    public void openWindow(String fxml) throws IOException{
+        Stage s = new Stage(StageStyle.UTILITY);
+        
+        Parent root = FXMLLoader.load(getClass().getResource(fxml));
+
+        Scene scene = new Scene(root);
+
+        s.setScene(scene);
+        s.show();
+    }
+    
+}
