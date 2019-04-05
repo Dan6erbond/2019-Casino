a2004d0aa61c0e503fc0eb53614b707d4c1d1df0
Dan6erbond <moravrav@gmail.com>
2019-04-05 10:42:30 +0200

    fixed blackjack

---
+++ Casino/src/main/java/ch/bbbaden/casino/blackjack/BlackJackController.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.bbbaden.casino.blackjack;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author marin
+ */
+public class BlackJackController implements Initializable {
+
+    @FXML
+    private Button putchip;
+    @FXML
+    private Button deletechip;
+    @FXML
+    private Button hit;
+    @FXML
+    private Button stand;
+    @FXML
+    private Button doubledown;
+    
+    int pool = 0;
+    int balance = 0;
+    
+    @FXML
+    private Label lpool;
+    @FXML
+    private Label lbalance;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void clickPutChips(ActionEvent event) {
+    }
+
+    @FXML
+    private void clickDeleteChips(ActionEvent event) {
+    }
+
+    @FXML
+    private void clickHit(ActionEvent event) {
+    }
+
+    @FXML
+    private void clickStand(ActionEvent event) {
+    }
+
+    @FXML
+    private void clickDouble(ActionEvent event) {
+    }
+
+    @FXML
+    private void clickHundred(ActionEvent event) {
+    }
+
+    @FXML
+    private void clickTwentyfive(ActionEvent event) {
+    }
+
+    @FXML
+    private void clickFive(ActionEvent event) {
+    }
+
+    @FXML
+    private void clickTwo(ActionEvent event) {
+        //this.lpool.textProperty().bind(balance.textProperty());
+    }
+    
+}
